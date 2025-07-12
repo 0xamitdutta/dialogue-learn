01884b5e756e419a5e6b03ccfe1eddbb6db41202
Amit Dutta <amitdutta2406@gmail.com>
2025-07-12 20:25:17 +0530

    Add recent companion sessions

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -1,12 +1,13 @@
 import {
     Table,
-    TableBody,
-    TableCaption,
-    TableCell,
+    TableBody, TableCell,
     TableHead,
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+import Link from "next/link";
+import Image from "next/image";
+import {getSubjectColor} from "@/lib/utils";
 
 interface CompanionsListProps {
     title: string;
@@ -21,19 +22,46 @@ const CompanionsList = ({ title, companions }: CompanionsListProps) => {
             <Table>
                 <TableHeader>
                     <TableRow>
-                        <TableHead className="w-[100px]">Invoice</TableHead>
-                        <TableHead>Status</TableHead>
-                        <TableHead>Method</TableHead>
-                        <TableHead className="text-right">Amount</TableHead>
+                        <TableHead className="w-[100px]">Lessons</TableHead>
+                        <TableHead>Subject</TableHead>
+                        <TableHead>Duration</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    <TableRow>
-                        <TableCell className="font-medium">INV001</TableCell>
-                        <TableCell>Paid</TableCell>
-                        <TableCell>Credit Card</TableCell>
-                        <TableCell className="text-right">$250.00</TableCell>
-                    </TableRow>
+                    {companions.map(({id, subject, name, topic, duration}) => (
+                        <TableRow key={id}>
+                            <TableCell>
+                                <Link href={`/companions/${id}`}>
+                                    <div className="flex items-center gap-2">
+                                        <div className="size-[72px] flex justify-center items-center max-md:hidden rounded-lg" style={{backgroundColor: getSubjectColor(subject)}}>
+                                            <Image src={`/icons/${subject}.svg`} alt={subject} height={35} width={35}></Image>
+                                        </div>
+                                    </div>
+
+                                    <div className="flex flex-col gap-2">
+                                        <p className="text-xl font-bold">{name}</p>
+                                        <p className="text-lg">{topic}</p>
+                                    </div>
+                                </Link>
+                            </TableCell>
+                            <TableCell>
+                                <div className="subject-badge max-md:hidden">
+                                    {subject}
+                                </div>
+                                <div className="size-[72px] flex justify-center items-center md:hidden rounded-lg" style={{backgroundColor: getSubjectColor(subject)}}>
+                                    <Image src={`/icons/${subject}.svg`} alt={subject} height={35} width={35}></Image>
+                                </div>
+                            </TableCell>
+
+                            <TableCell>
+                                <div className="flex items-center gap-2">
+                                    <p className="text-xl">{duration} {' '}</p>
+                                    <span className="max-md:hidden">mins</span>
+                                    <Image src="/icons/clock.svg" alt="minutes" height={15} width={15} className="md:hidden"></Image>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    ))}
                 </TableBody>
             </Table>
         </article>
